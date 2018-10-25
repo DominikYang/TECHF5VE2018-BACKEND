90afb71501cb9c59ce18a68410c5eef6e2ab3c1e
cdfbigbool <15606108957@163.com>
2018-10-25 21:25:39 +0800

    finsh

--- task01/成冬樊task01/Validate/manager/Manager.java
+++ task01/成冬樊task01/Validate/manager/Manager.java
@@ -1,14 +1,21 @@
-package Staff.commonemployee;
-import Staff.member.Member;
+﻿package Validate.manager;
+
+import entity.Member;
 
 import java.util.Scanner;
-
-public class CommonEmployee extends Member{
+/**
+ *
+ * @author chengdongfan;
+ * @version V1.0
+ * @date 2018/10/24
+ * @since 1.8
+ */
+public class Manager extends Member {
     Scanner inputData = new Scanner(System.in);
 
     //工资范围
     public void setSalary(double salary) {
-        if (salary > 4000 && salary < 6000) {
+        if (salary > 8000 && salary < 15000) {
             super.setSalary(salary);
         } else {
             System.out.println("输入薪水有误,请重新输入薪水:");
@@ -18,6 +25,3 @@ public class CommonEmployee extends Member{
 
     }
 }
-
-
-
